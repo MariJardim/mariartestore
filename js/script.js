502408c32f5745e6c71080a5b478f60c929738e6
Mari <mari-jen@hotmail.com>
2022-06-21 17:50:25 -0300

    adicionados eventos de  mouseup, mousemove e mousedown ao slide

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-// import Slide from './slide.js';
+
 import initDropDownMenu from './dropdown-menu.js';
 import activeMenu from './menu.js';
+import initSlide from './slide.js';
 
 
 
 
 // initSlide();
 initDropDownMenu('[data-dropdown]');
+initSlide();
--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,23 @@
-export default function Slide(){
-  const slides = document.querySelector('.slides');
+export default function initSlide(){
+  const slide = document.querySelector('.slide');
   const wrapper = document.querySelector('.wrapper')
 
-
+  function onStart(e){
+    e.preventDefault();
+    wrapper.addEventListener('mousemove', onMove);
+    
+    
+   }
+   function onMove(e){
+    
+   }
+   function onEnd(e){
+    
+    wrapper.removeEventListener('mousemove', onMove)
+   }
+   function addSlideEvents(){
+    wrapper.addEventListener('mousedown', onStart);
+    wrapper.addEventListener('mouseup', onEnd)
+   }
+   addSlideEvents()
 }
