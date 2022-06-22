931faeffa529573553deba6682e4f1d8082c2a83
Mari <mari-jen@hotmail.com>
2022-06-22 16:20:25 -0300

    eventos de touch adicionados ao slide

--- js/slide.js
+++ js/slide.js
@@ -6,33 +6,44 @@ export default function initSlide(){
     startX: 0,
     movement: 0
   }
+  
 function moveSlide(xDistance){
   distance.movePosition = xDistance;
   slide.style.transform = `translate3d(${xDistance}px, 0, 0)`
 }
-
 
   function updatePosition(clientX){
     distance.movement = (distance.startX - clientX) * 1.6;
     return distance.finalPosition - distance.movement 
  }
   function onStart(e){
-    e.preventDefault();
-    distance.startX = e.clientX;
-    wrapper.addEventListener('mousemove', onMove);
+    let movetype;
+    if(e.type === 'mousedown'){
+      e.preventDefault();
+      distance.startX = e.clientX;
+      movetype = 'mousemove'
+    } else{
+      distance.startX = e.changedTouches[0].clientX;
+      movetype = 'touchmove'
+    }
+    wrapper.addEventListener(movetype, onMove);
    }
    function onMove(e){
-   const finalPosition = updatePosition(e.clientX);
+    const pointerPosition = (e.type === 'mousemove' ? e.clientX : e.changedTouches[0].clientX)
+   const finalPosition = updatePosition(pointerPosition);
    moveSlide(finalPosition)
    }
 
    function onEnd(e){
-    wrapper.removeEventListener('mousemove', onMove);
+    const movetype = (e.type === 'mouseup' ? 'mousemove' : 'touchmove')
+    wrapper.removeEventListener(movetype, onMove);
     distance.finalPosition = distance.movePosition
    }
    function addSlideEvents(){
     wrapper.addEventListener('mousedown', onStart);
+    wrapper.addEventListener('touchstart', onStart);
     wrapper.addEventListener('mouseup', onEnd)
+    wrapper.addEventListener('touchend', onEnd)
    }
    addSlideEvents()
 }
